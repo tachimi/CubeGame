d17aecfd5b5010501b345089f2f337476431b57a
tachimi <romsteron@gmail.com>
2023-04-29 16:05:16 +0300

    Initial commit

---
+++ Assets/Задания/Задача 6/Level6.cs
@@ -0,0 +1,19 @@
+using System.Threading;
+using UnityEngine;
+
+public class Level6 : MonoBehaviour
+{
+    [SerializeField] 
+    private GameObject _obstacle;
+    
+    private float _speed = 5;
+
+    private void Update()
+    {
+        var step = Time.deltaTime * _speed;
+        var transformPosition = transform.position;
+        transformPosition.x += step;
+
+        transform.position = transformPosition; 
+    }
+}
